85c6ce92fd034cde1312e9f86ff6b38c5a5fe2c6
joacuba <jqcuba@gmail.com>
2025-03-24 03:23:39 -0500

    fix(scripts): Remove the file link if exist before create symlink

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+[ -e ~/.vimrc ] && rm ~/.vimrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
+
+[ -e ~/.config/nvim ] && rm -rf ~/.config/nvim
 ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
+
+[ -e ~/.config/alacritty ] && rm -rf ~/.config/alacritty
 ln -s ~/.dotfiles/.config/alacritty ~/.config/alacritty
+
+[ -e ~/.config/ghostty ] && rm -rf ~/.config/ghostty
 ln -s ~/.dotfiles/.config/ghostty ~/.config/ghostty
+
+[ -e ~/.tmux.conf ] && rm ~/.tmux.conf
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
 echo "Symlinks created successfully!"
