995b158780b34d569e369685f5ccf98491298625
joacuba <jqcuba@gmail.com>
2026-02-08 11:38:16 -0500

    feat: added keymaps to git plugin (vim fugitive)

--- .config/nvim/lua/jc/plugins/fugitive.lua
+++ .config/nvim/lua/jc/plugins/fugitive.lua
@@ -1,3 +1,20 @@
 return {
 	"tpope/vim-fugitive",
+	config = function()
+		vim.keymap.set("n", "<leader>gs", function()
+			vim.cmd("Git status")
+		end, { desc = "See git status" })
+
+		vim.keymap.set("n", "<leader>gp", function()
+			vim.cmd("Git push")
+		end, { desc = "git push" })
+
+		vim.keymap.set("n", "<leader>gc", function()
+			vim.cmd("Git commit")
+		end, { desc = "git commit" })
+
+		vim.keymap.set("n", "<leader>go", function()
+			vim.cmd("Git")
+		end, { desc = "Open fugitive (git)" })
+	end,
 }
