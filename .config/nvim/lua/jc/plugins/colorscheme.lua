b3858fb5a55d8d936a834f4fd0fc0f2e55243d6a
joacuba <jqcuba@gmail.com>
2026-03-15 02:54:35 -0500

    style: change color scheme

--- .config/nvim/lua/jc/plugins/colorscheme.lua
+++ .config/nvim/lua/jc/plugins/colorscheme.lua
@@ -1,15 +1,29 @@
 return {
-	"aktersnurra/no-clown-fiesta.nvim",
-	priority = 1000,
-	config = function()
-		require("no-clown-fiesta").setup({
-			theme = "dark", -- supported themes are: dark, dim, light
-			transparent = true,
-		})
-		vim.cmd([[colorscheme no-clown-fiesta]])
-	end,
+	"webhooked/kanso.nvim",
 	lazy = false,
+	priority = 1000,
+	opts = {
+		theme = "zen",
+		transparent = true,
+		terminalColors = true,
+	},
+	init = function()
+		vim.cmd("colorscheme kanso")
+	end,
 }
+
+--return {
+--	"aktersnurra/no-clown-fiesta.nvim",
+--	priority = 1000,
+--	config = function()
+--		require("no-clown-fiesta").setup({
+--			theme = "dark", -- supported themes are: dark, dim, light
+--		})
+--		vim.cmd([[colorscheme no-clown-fiesta]])
+--	end,
+--	lazy = false,
+--}
+
 --return {
 --	"metalelf0/black-metal-theme-neovim",
 --	lazy = false,
@@ -23,6 +37,7 @@ return {
 --		require("black-metal").load()
 --	end,
 --}
+
 --return {
 --	"bluz71/vim-moonfly-colors",
 --	name = "moonfly",
@@ -30,20 +45,6 @@ return {
 --	priority = 1000,
 --	config = function()
 --		vim.cmd([[colorscheme moonfly]])
---	end,
---}
-
---return {
---	"webhooked/kanso.nvim",
---	lazy = false,
---	priority = 1000,
---	opts = {
---		theme = "zen",
---		transparent = true,
---		terminalColors = true,
---	},
---	init = function()
---		vim.cmd("colorscheme kanso")
 --	end,
 --}
 
